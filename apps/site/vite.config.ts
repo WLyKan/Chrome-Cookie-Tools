0bdabf7c9ee0814811da09fa3b084b57d9fc6be7
wangyikan <wangyikan@supcon.com>
2026-03-17 14:42:58 +0800

    feat: 更新 vite 配置

--- apps/site/vite.config.ts
+++ apps/site/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
-  base: "/chrome-cookie-tools/",
+  base: "/Chrome-Cookie-Tools/",
   plugins: [react()],
   server: {
     port: 5173,
